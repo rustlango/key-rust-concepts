895477306808c9b468ce95015c6bddf5ec0bccd8
Lemuel <wyntonsauls@gmail.com>
2022-09-27 12:04:06 +0200

    flow control statements completed

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 fn main() {
     println!("Hello, world!");
     basic_functions::functions_in_rust();
+    flow_control_statements::flow_control_statements();
 }
 
 mod basic_functions { pub fn functions_in_rust() {
     println!("Hello, world!");
 
     second_function();
-
+    
 }
 
 fn second_function() {
@@ -15,3 +16,20 @@ fn second_function() {
 }
 }
 
+// FLOW CONTROL STATEMENTS
+
+mod flow_control_statements {
+    pub fn flow_control_statements() {
+        let number = 65;
+
+        if number % 7 == 0 {
+            println! ("number is divisible by 7");
+        } else if number % 5 ==0 {
+            println!("number is divisible by 5");
+        } else if number % 2 == 0 {
+            println!("number is divisible by 2");
+        } else {
+            println!{"number is not divisible by 7, 5 and 2"};
+        }
+    }
+}
