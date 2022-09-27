abc7cdcb8acca5dee71a382448800de38b154c70
Lemuel <wyntonsauls@gmail.com>
2022-09-27 11:55:25 +0200

    functions in rust

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Hello, world!");
+    functions_in_rust();
+}
+
+fn functions_in_rust() {
+    println!("Hello, world!");
+
+    second_function();
+
+}
+
+fn second_function() {
+    println!("A second function");
+}
