6c8dccfe99a68654a8f1013415453be9ae546aac
Lemuel <wyntonsauls@gmail.com>
2022-09-28 16:17:39 +0200

    bug fix: correct return types specified for hashmaps and vectors

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,9 @@ fn main() {
     enum_and_match::value_in_cents(enum_and_match::Coin::Quarter);
 
     // Vectors and HashMaps
-    println!("The v vector looks like this {:?}", vecs_and_hashmaps::vecs::v);
+    println!("The v vector looks like this {:?}", vecs_and_hashmaps::vecs::vectors_in_rust());
+
+    println!("The scores hashmap looks like this {:?}", vecs_and_hashmaps::hashmaps::hashmapping());
 }
 
 mod basic_functions { pub fn functions_in_rust() {
@@ -106,20 +108,36 @@ mod enum_and_match {
 
 mod vecs_and_hashmaps {
     pub mod vecs {
+
+        pub fn vectors_in_rust() -> Vec<i32> {
+
         // vectors are growable arrays
-        let mut v: Vec<i32> = vec!::new();
-        v.push(5);
-        v.push(6);
-        v.push(7);
-        v.push(8);
+        // always ensure you use let statements wrapped in functions
+        let mut v: Vec<i32> = Vec::new();
+            v.push(5);
+            v.push(6);
+            v.push(7);
+            v.push(8);
+
+            return v;
+        }
     }
 
     pub mod hashmaps {
         use std::collections::HashMap;
 
-        let mut scores = HashMap::new();
+        pub fn hashmapping() -> HashMap<String, f64> {
+        
+        // always ensure you use let statements wrapped in functions
+        // basic hashmap implmentation for Polkadot and Kusama token balances.
+        // So you can indeed use hashamps for crypto_wallet implmentations
+    
+        let mut crypto_balances = HashMap::new();
 
-        scores.insert(String::from("Blue"), 10);
-        scores.insert(String::from("Blue"), 50);
+        crypto_balances.insert(String::from("DOT"), 105665.546655797706533222);
+        crypto_balances.insert(String::from("KSM"), 4668.857054878686707642);
+
+        return crypto_balances;
     }
+ }
 }
