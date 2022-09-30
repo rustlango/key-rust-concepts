b26d756e167579b0ca5a85a8abaf61cd2f7f992d
Lemuel <wyntonsauls@gmail.com>
2022-09-30 13:32:56 +0200

    if1  exercise completed - fix: added in if arms to return the bigger number

--- exercises/functions/functions5.rs
+++ exercises/functions/functions5.rs
@@ -1,7 +1,7 @@
 // functions5.rs
 // Execute `rustlings hint functions5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// DONE WITH EXERCISE
 
 fn main() {
     let answer = square(3);
