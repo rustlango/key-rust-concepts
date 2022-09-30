bfd4226e9eae08f31493c8b340d799ad1db555a0
Lemuel <wyntonsauls@gmail.com>
2022-09-30 12:23:10 +0200

    exerice variables pt 1 completed

--- exercises/variables/variables1.rs
+++ exercises/variables/variables1.rs
@@ -2,7 +2,7 @@
 // Make me compile!
 // Execute `rustlings hint variables1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// SUCCESSFULLY COMPLETED EXERCISE
 
 fn main() {
     let x = 5;
