5a0a90cc8dc2fb72f02c9dafc6f2fb72050643b8
Lemuel <wyntonsauls@gmail.com>
2022-09-30 12:39:30 +0200

    variables 6 exercise completed - const type annotated to run code - const can be decalred outside of functions unlike let

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,9 +1,11 @@
 // variables6.rs
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// DONE WITH EXERCISE
 
-const NUMBER = 3;
+// const bindings can be decalred outside of a function as oppose to let bindings.
+// however, type must always be annotated when declaring conts bindings
+const NUMBER: u8 = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
