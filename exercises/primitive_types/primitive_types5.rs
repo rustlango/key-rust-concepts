417bd50266059d90976d6a1f59bb097a5cd45af1
Lemuel <wyntonsauls@gmail.com>
2022-09-30 14:11:38 +0200

    p_t5  exercise completed - fix: DESTRUCTURED TUPLE WITH CORRECT NAME AND AGE TAGS

--- exercises/primitive_types/primitive_types5.rs
+++ exercises/primitive_types/primitive_types5.rs
@@ -2,11 +2,12 @@
 // Destructure the `cat` tuple so that the println will work.
 // Execute `rustlings hint primitive_types5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// DONE WITH EXERCISE
 
 fn main() {
     let cat = ("Furry McFurson", 3.5);
-    let /* your pattern here */ = cat;
+    // TUPLE DESTRUCTING BY TAGGING THE EACH ELEMENT
+    let (name, age) = cat;
 
     println!("{} is {} years old.", name, age);
 }
