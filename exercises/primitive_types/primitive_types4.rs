415fe57cdeeac089a7f0cdc4e8b00f0649970089
Lemuel <wyntonsauls@gmail.com>
2022-09-30 14:06:59 +0200

    p_t5  exercise completed - fix: sliced a array by using a immutable reference to the original array in order to create the nice_slice array

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -2,7 +2,7 @@
 // Get a slice out of Array a where the ??? is so that the test passes.
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// DONE WITH EXERCISE
 
 #[test]
 fn slice_out_of_array() {
