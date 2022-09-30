c205faf0eb3e9a961dfc4ffb665b4dd98c8d372b
Lemuel <wyntonsauls@gmail.com>
2022-09-30 14:01:28 +0200

    p_t3  exercise completed - fix: created array container using shorthand

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,14 +2,16 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// DONE WITH EXERCISE
 
 fn main() {
-    let a = ???
+    let a = ["Thanks"; 100];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
     }
+
+    println!("array {:?}", a.len());
 }
