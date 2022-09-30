b8c63eba70aa27ae878e10cc3e185a49832f4aaf
Lemuel <wyntonsauls@gmail.com>
2022-09-30 12:52:06 +0200

    functions 2 exercise completed - const type annotated to run code - fix: cannotated function param with type

--- exercises/functions/functions2.rs
+++ exercises/functions/functions2.rs
@@ -1,13 +1,14 @@
 // functions2.rs
 // Execute `rustlings hint functions2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// EXERCISE COMPLETED
 
 fn main() {
     call_me(3);
 }
 
-fn call_me(num:) {
+// Annotated function param with type u8 (alwaysdo this even with retrun types)
+fn call_me(num: u8) {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
