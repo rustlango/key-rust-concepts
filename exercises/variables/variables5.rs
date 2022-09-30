2c49da68ee9019b39e932fed51ecbd5b4a5b8507
Lemuel <wyntonsauls@gmail.com>
2022-09-30 12:35:50 +0200

    variables 5 exercise completed - let keyword added to shadow previous binding with new type

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,12 @@
 // variables5.rs
 // Execute `rustlings hint variables5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// DONE WITH THIS EXERCISE
 
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
+    // inserted let keyword to shadow binding for type conversion
+    let number = 3; // don't rename this variable
     println!("Number plus two is : {}", number + 2);
 }
