1c259ce4be9aa12d396c4d0ec707d223b7bdfeed
Lemuel <wyntonsauls@gmail.com>
2022-09-30 13:57:43 +0200

    p_t2  exercise completed - fix: completed CHAR BIDNIN DECLARATION FOR YOUR_CHARACTER IF ARM

--- exercises/primitive_types/primitive_types2.rs
+++ exercises/primitive_types/primitive_types2.rs
@@ -2,7 +2,7 @@
 // Fill in the rest of the line that has code missing!
 // No hints, there's no tricks, just get used to typing these :)
 
-// I AM NOT DONE
+// DONE WITH EXERCISE
 
 fn main() {
     // Characters (`char`)
