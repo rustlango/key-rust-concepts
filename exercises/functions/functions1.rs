fce15ba61e33e6aecf3b596cef28b9836931bcbe
Lemuel <wyntonsauls@gmail.com>
2022-09-30 12:42:43 +0200

    functions 1 exercise completed - const type annotated to run code - fix: created call_me() function outside of main because it was not decalred

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,8 +1,13 @@
 // functions1.rs
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// DONE WITH EXERCISE
 
 fn main() {
     call_me();
+}
+
+// ADDED IN THE callme() function becuase it did not exist
+fn call_me() {
+
 }
