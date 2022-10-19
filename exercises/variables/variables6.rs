7082b80034f062e8845042a86c73872b5831e8e0
Lemuel Solomon <49029145+alvorithm@users.noreply.github.com>
2022-10-19 13:38:55 +0200

    Update variables6.rs

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,7 +1,7 @@
 // variables6.rs
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a hint.
 
-// DONE WITH EXERCISE
+// I AM DONE
 
 // const bindings can be decalred outside of a function as oppose to let bindings.
 // however, type must always be annotated when declaring conts bindings
