6772cce432c551fe8155f2e1d0ccd92519684f27
Lemuel Solomon <49029145+alvorithm@users.noreply.github.com>
2022-10-19 14:31:48 +0200

    Update move_semantics5.rs

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -3,13 +3,16 @@
 // adding, changing or removing any of them.
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
     let mut x = 100;
     let y = &mut x;
-    let z = &mut x;
     *y += 100;
+    let z = &mut x;
     *z += 1000;
     assert_eq!(x, 1200);
+    // print to output screen before removing "I AM NOT DONE" line
+    // Not needed - just for feedback purposes before completing exercise
+    println!("value of x is {}", x);
 }
