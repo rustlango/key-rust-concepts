f4f9d6e65315c5cfe8953ca44ab07a98833f0872
Lemuel Solomon <49029145+alvorithm@users.noreply.github.com>
2022-10-19 13:42:14 +0200

    Update functions2.rs

--- exercises/functions/functions2.rs
+++ exercises/functions/functions2.rs
@@ -1,13 +1,13 @@
 // functions2.rs
 // Execute `rustlings hint functions2` or use the `hint` watch subcommand for a hint.
 
-// EXERCISE COMPLETED
+// I AM DONE
 
 fn main() {
     call_me(3);
 }
 
-// Annotated function param with type u8 (alwaysdo this even with retrun types)
+// Annotated function param with type u8 (always do this even with retrun types)
 fn call_me(num: u8) {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
